e520695a1127cef1774029ffca51e1a36fa393d0
noeLogic <noeltwilliams@hotmail.com>
2015-09-15 10:27:31 +0100

    My First Commit Roll-A-Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() // guaranteed to run after all items have been processed in update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
